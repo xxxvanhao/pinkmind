29f47f28b8b34d957b5e748b7ffac60da4cfd9e7
xxxvanhao <kidzvhb3@gmail.com>
2019-06-03 09:31:17 +0700

    update

---
+++ src/app/projects/projectsetting/projectsetting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projectsetting',
+  templateUrl: './projectsetting.component.html',
+  styleUrls: ['./projectsetting.component.css']
+})
+export class ProjectsettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
