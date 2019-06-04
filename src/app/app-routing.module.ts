5c6de56f2321352134cd891e81457a2aab3b1b49
xxxvanhao <kidzvhb3@gmail.com>
2019-06-04 13:35:38 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
 import { BodyComponent } from './dashboard/body.component';
 import { ProjectsComponent } from './projects/projects.component';
-
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './Account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: 'account', pathMatch: 'full'},
+  {path: 'account', component: AccountComponent, children : [
+    {path: '', redirectTo: 'login', pathMatch: 'prefix'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
+  ]},
   {path: 'dashboard', component: BodyComponent},
   {path: 'projects', component: ProjectsComponent}
 ];
